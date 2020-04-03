57aec4166d15ab81dccdc7f68f159f982ef74392
JulieLeNguyen <nguyenjul17@gmail.com>
2020-04-02 22:05:31 -0400

    first commit!

---
+++ randompairing.py
@@ -0,0 +1,11 @@
+# buff bot script to randomly pair people and create dm #
+
+from slackclient import SlackClient
+SLACK_API_TOKEN = 'xoxb-4890906412-1027205464017-tbf5SYrvdx4alt3Pl7uqNXDm'
+
+# bot spam channel id C06HLL9CL
+
+message-"hello"
+slack.chat.post_message('#botspam',message)
+
+
