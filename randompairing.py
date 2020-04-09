52f9c427159a114269a85bca94a9e2aaf25c6032
JulieLeNguyen <nguyenjul17@gmail.com>
2020-04-09 01:37:40 -0400

    getting members from #buff!

--- randompairing.py
+++ randompairing.py
@@ -3,14 +3,24 @@
 import os
 import slack
 
+
 slack_api_token = os.environ.get('slack_api_token')
 print('slack_api_token')
 
-client = slack.WebClient(token=os.environ.get('slack_api_token'))
-response = client.chat_postMessage(
-    channel='#botspam',
-    text="Hellooo!")
+#client = slack.WebClient(token=os.environ.get('slack_api_token'))
+#response = client.chat_postMessage(
+  #  channel='#buff',
+   # text="testingg!")
 
+slack_token = os.environ["slack_api_token"]
+client = slack.WebClient(slack_token)
 
+rolecall= client.conversations_members(channel="C0113BP18RG")
+
+print (rolecall)
+
+#group_open , create group dm
+#im_created, dm was created
+#message.im message posted in direct channel
 
 
