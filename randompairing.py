d19184fff3be833c01ac84a98f8b8d0eacb88411
JulieLeNguyen <nguyenjul17@gmail.com>
2020-04-12 13:06:06 -0400

    created first dm with a pair from #buff!

--- randompairing.py
+++ randompairing.py
@@ -2,25 +2,42 @@
 
 import os
 import slack
-
+import random
 
 slack_api_token = os.environ.get('slack_api_token')
-print('slack_api_token')
+#print(slack_api_token)
+client = slack.WebClient(token=os.environ.get('slack_api_token'))
 
-#client = slack.WebClient(token=os.environ.get('slack_api_token'))
+# to message in a channel
 #response = client.chat_postMessage(
-  #  channel='#buff',
-   # text="testingg!")
+ #   channel='#buff',
+  #  text="testingg!")
 
-slack_token = os.environ["slack_api_token"]
-client = slack.WebClient(slack_token)
+#find bot ID
+bot_info=client.bots_info()
+print(bot_info)
 
-rolecall= client.conversations_members(channel="C0113BP18RG")
+# to get list of members in channel
+rolecall= client.conversations_members(channel="C0113BP18RG")['members']
+print(rolecall)
 
-print (rolecall)
+#next step, generate 2 indices and then grab people from those indices
 
-#group_open , create group dm
-#im_created, dm was created
-#message.im message posted in direct channel
+length=int(len(rolecall))
+print(length)
 
+firsthalf=rolecall[:length//2]
+secondhalf=rolecall[length//2:]
+#print(firsthalf)
+#print(secondhalf)
 
+#dming the pairs
+#buff bot userid is U010T61DN0H
+howdy=client.conversations_open(users=["UD86L3M54","U23L9EKQ8"])
+print(howdy)
+
+response = client.chat_postMessage(
+    channel='G011KGP5YTY',
+    text="Hello! It's time to get buff! (this is a test)")
+
+#workout list
