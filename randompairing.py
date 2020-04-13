03fd2d1d0285f4c1644cae34d4115b3b7767403f
JulieLeNguyen <nguyenjul17@gmail.com>
2020-04-13 00:12:19 -0400

    created a while loop to pair up users!

--- randompairing.py
+++ randompairing.py
@@ -1,40 +1,21 @@
-# buff bot script to randomly pair people and create dm #
+# buffbot script to randomly pair people and create dm #
 
 import os
 import slack
 import random
 
 slack_api_token = os.environ.get('slack_api_token')
+buff_channel_id = os.environ.get('buff_channel_id')
+buffbot_id=os.environ.get('buffbot_id')
 #print(slack_api_token)
+#print(buff_channel_id)
+#print(buffbot_id)
 client = slack.WebClient(token=os.environ.get('slack_api_token'))
 
 # to message in a channel
 #response = client.chat_postMessage(
    #channel='#buff',
-    #text="this isnt one punch man lmao")
-
-# to get list of members in channel
-rolecall= client.conversations_members(channel="C0113BP18RG")['members']
-print(rolecall)
-
-#next step, generate 2 indices and then grab people from those indices
-
-length=int(len(rolecall))
-print(length)
-
-firsthalf=rolecall[:length//2]
-secondhalf=rolecall[length//2:]
-
-#buff bot userid is U010T61DN0H
-del secondhalf[5]
-
-print("firsthalf:",firsthalf)
-print("secondhalf",secondhalf)
-
-#get channel id from a pair
-
-howdy=client.conversations_open(users=[random.choice(firsthalf),random.choice(secondhalf)])
-print(howdy)
+    # text="testing!")
 
 #workout list
 workout_list=[
@@ -49,20 +30,72 @@ workout_list=[
     '50 squats',
     '30 pushups',
     'play some ping pong',
-    'do 5 jumping jacks every time you hear thinkpads'
+    'do 5 jumping jacks every time you hear thinkpads',
+    'try out a weightlifting session',
+    'plank competition',
+    '15 burpees',
+    '2 mile run'
 ]
-
 workout1=random.choice(workout_list)
 workout2=random.choice(workout_list)
+
+workout1_index=workout_list.index(workout1)
+workout2_index=workout_list.index(workout2)
+del workout_list[workout1_index]
+del workout_list[workout2_index]
+
 workout3=random.choice(workout_list)
 
-#message pair
-response = client.chat_postMessage(
-   channel=howdy['channel']['id'],
-   text="Hello! It's time to get *buff*! _(this is a test)_ \n"
-         "Your workout options are:\n>" +workout1 + "\n>" +workout2 + "\n>" +workout3)
+# get list of members in channel
+rolecall= client.conversations_members(channel=buff_channel_id)['members']
+#print(rolecall)
+
+random.shuffle(rolecall)
+print("list after shuffle:", rolecall)
+
+#del buffbot from users in channel
+botindex=rolecall.index(buffbot_id)
+#print(botindex)
+del rolecall[botindex]
+
+#generate 2 list and then pair people from those lists
+length=int(len(rolecall))
+#print(length)
+firsthalf=rolecall[:length//2]
+secondhalf=rolecall[length//2:]
+#print("firsthalf:",firsthalf)
+#print("secondhalf",secondhalf)
+
+#slide into those dms
+#check to see rolecall even or odd
+if (length %2)==0:
+    max = len(firsthalf)
+    n = 0
+    while n < max:
+        warmup = client.conversations_open(users=[firsthalf[n], secondhalf[n]])
+        print(warmup)
+            response = client.chat_postMessage(
+             channel=howdy['channel']['id'],
+             text="Hello! It's time to get *buff*! _(this is a test)_ \n"
+                  "Your workout options are:\n>" +workout1 + "\n>" +workout2 + "\n>" +workout3)
+        n += 1
+else:
+    max = len(secondhalf)
+    print(max)
+    n=0
+    while n<max-2:
+        warmup = client.conversations_open(users=[firsthalf[n], secondhalf[n]])
+        print(warmup)
+         response = client.chat_postMessage(
+           channel=howdy['channel']['id'],
+           text="Hello! It's time to get *buff*! _(this is a test)_ \n"
+                "Your workout options are:\n>" +workout1 + "\n>" +workout2 + "\n>" +workout3)
+        n += 1
+    warmup = client.conversations_open(users=[firsthalf[max-2], secondhalf[max-2], secondhalf[max-1]])
+    print(warmup)
+
 
 #checking in
 #response = client.chat_postMessage(
-    #channel=
-    #text= "Checking in! Did you get a chance to workout?"
+   # channel=howdy['channel']['id']
+   # text= "Checking in! Did you get a chance to workout?")
